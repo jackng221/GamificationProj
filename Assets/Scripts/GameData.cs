7a2401dc38fa18a3334a91b91f0fd01cead1e5dd
jackng221 <jackng1233@gmail.com>
2022-03-22 05:40:59 +0800

    Init

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    private string[] subjects = { "A", "B", "C" };
+    [SerializeField] Sprite[] subjectsIcons;
+
+    public string[] getSubjects()
+    {
+        return subjects;
+    }
+}
---
+++ Assets/Scripts/SessionControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionControl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] sessions;
+
+
+    public void Start()
+    {
+        sessions[0].SetActive(true);
+        if (sessions.Length > 1)
+        {
+            for (int i = 1; i < sessions.Length; i++)
+            {
+                sessions[i].SetActive(false);
+            }
+        }
+    }
+    public bool findSession(string session)
+    {
+        for (int i = 0; i < sessions.Length; i++)
+        {
+            if (sessions[i].name.Equals(session))
+            {
+                return true;
+            }
+        }
+        Debug.Log("session not found");
+        return false;
+    }
+    public void goToSession(string session)
+    {
+        if (findSession(session))
+        {
+            for (int i = 0; i < sessions.Length; i++)
+            {
+
+            }
+        }
+    }
+}
